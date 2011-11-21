aed5bd42d5dc5f10fce030bc236d6d7255ec5c10
Tony Daly <tony.daly@me.com>
2011-11-21 13:47:04 +0000

    Removed index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 FormulatronApp::Application.routes.draw do
+  resources :forms
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +49,7 @@ FormulatronApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'forms#index'
 
   # See how all your routes lay out with "rake routes"
 
