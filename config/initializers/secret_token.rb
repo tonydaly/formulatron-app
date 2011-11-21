1a59e8dc4d8256cc0f8ddd4d91e42db223b860bd
Tony Daly <tony.daly@me.com>
2011-11-21 13:30:10 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FormulatronApp::Application.config.secret_token = '9af9e85f8156be4b29c1eabd3e2a1bdcbcd67ea15a869f1c4361cb5645c2886e7e606da5c3f4bfed6ce15257b2783dcdc7dd9813d9daf7e583aa8a918cd6d9fa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FormulatronApp::Application.config.session_store :cookie_store, key: '_formulatron-app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FormulatronApp::Application.config.session_store :active_record_store
